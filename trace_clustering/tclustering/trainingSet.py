bc9db6c9eeccf2b509f7debd2ce887adedd03497
Unknown <jurgenlentz26@gmail.com>
2020-12-07 00:41:09 +0100

    trainingSet class and testing

--- trace_clustering/tclustering/trainingSet.py
+++ trace_clustering/tclustering/trainingSet.py
@@ -1,14 +1,25 @@
+import random
 
 
 class TrainingSet:
-    """
-    sample_set - list of traces - the attributes of the eventlog are not changed
-    """
     def __init__(self, sample_set):
+        """
+        :param sample_set: list of traces - the attributes of the eventlog are not changed
+        """
+        training_set_help = list()
         self.training_set = list()
-        len_train_set = round(len(sample_set) / 2)
-        for i in range(len_train_set):
+        len_train_set = round(len(sample_set) * 0.5)
+        i = 0
+        while i < len_train_set:
+            help_var = random.randint(0, len(sample_set) - 1)
+            if help_var not in training_set_help:
+                training_set_help.append(help_var)
+                i += 1
+        for i in training_set_help:
             self.training_set.append(sample_set[i])
 
     def get_training_set(self):
+        """
+        :return: Returns the training set.
+        """
         return self.training_set
